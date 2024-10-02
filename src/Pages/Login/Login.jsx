0b617c2e3b27c97adec9c0c29c45bbbf91186a37
Rahmadjon0038 <rahmadjonabdullayev@gmail.com>
2024-10-02 18:19:58 +0500

    davom etmoqda

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-import { Wrapper } from './style'
-
+import React, { useEffect, useState } from 'react'
+import { Container, FormContainer, Title, InputContainer, Label, Input, SubmitButton, Form } from '../Register/styled'
+import axios from 'axios'
 function Login() {
+
+  const [loginData, setLoginData] = useState(
+    {
+      email: ",",
+      password: "",
+    }
+  )
+  const handleChange = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value })
+  }
+
+
   return (
-    <Wrapper>Login</Wrapper>
+    <Container>
+      <FormContainer>
+        <Title>Sign in</Title>
+        <Form  >
+
+          <InputContainer>
+            <Label>
+              Email*
+              <Input
+                type="email"
+                name="email"
+                maxLength="254"
+                required
+                // value={formData.email}
+                onChange={handleChange}
+              />
+            </Label>
+          </InputContainer>
+
+          <InputContainer>
+            <Label>
+              Password*
+              <Input
+                type="password"
+                name="password"
+                maxLength="128"
+                minLength="1"
+                required
+                // value={formData.password}
+                onChange={handleChange}
+              />
+            </Label>
+          </InputContainer>
+          <SubmitButton type='sumbit'>Log in</SubmitButton>
+        </Form>
+
+      </FormContainer>
+    </Container>
   )
 }
 
