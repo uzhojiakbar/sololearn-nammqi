a7bf803080fb417d527d742a1c40a87060220d1e
Rahmadjon0038 <rahmadjonabdullayev@gmail.com>
2024-10-07 11:02:58 +0500

    response

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -1,74 +1,77 @@
 import React from "react";
-import {
-  Card,
-  Wrapper,
-  WrapperBox,
-  Image,
-  Title,
-  Text,
-  Btn,
-  Btn2,
-  Wrappdesc,
-  WrapperChild,
-  Card2,
-  WrapItem,
-  Card2Desc,
-  ImgConteiner,
-  ImgConteinerItem1,
-  ImgConteinerItem2,
-  ImgConteinerItem3,
-} from "./style";
 import { card2, card2later, CardsData } from "../../Utils/coruselData";
 
 function Cards() {
   return (
-    <Wrapper>
-      <WrapperBox>
+    <div className="bg-[#eaf0f3] w-full">
+      {/* WrapperBox */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-12 mx-12 mb-8">
         {CardsData?.map((item) => (
-          <Card key={item.id}>
-            <Image src={item.image} />
-            <Wrappdesc>
-              <div>
-                <Title>{item.name}</Title>
-                <Text>{item.desc}</Text>
-              </div>
-              <Btn>{item.btn}</Btn>
-            </Wrappdesc>
-          </Card>
-        ))}
-      </WrapperBox>
 
-      <WrapperChild>
+          <div key={item.id} className="bg-white rounded-lg px-2 pb-6 flex flex-col  text-center ">
+            <img src={item.image} className="w-[110%] " alt="" />
+            <div className="flex flex-col justify-between items-center h-full"><div>
+              <div className="text-[#2d3846] text-3xl font-bold mt-4">{item.name}</div>
+              <p className="text-[#6b7f99] px-2 mt-4  sm:leading-7 ">{item.desc}</p>
+            </div>
+              <button className="w-[223px]  h-12 text-white bg-[#2493df]  font-bold rounded mt-6 hover:bg-[#58b5f3]">
+                {item.btn}
+              </button>
+            </div>
+          </div>
+
+        ))}
+        {card2later?.map((item) => (
+          <div key={item.id} className="bg-white rounded-lg px-2 lg:fixed pb-6 flex flex-col  text-center ">
+            <img src={item.image} className="w-[110%] " alt="" />
+            <div className="flex flex-col justify-between items-center h-full"><div>
+              <div className="text-[#2d3846] text-3xl font-bold mt-4">{item.name}</div>
+              <p className="text-[#6b7f99] px-2 mt-4 leading-7">{item.desc}</p>
+            </div>
+              <button className="w-[223px] h-12  text-white bg-[#40BF9C] font-bold rounded mt-6 hover:bg-[#4febbe]">
+                {item.btn}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+
+
+      <div className=" gap-8 mx-12 mt-5 pb-12 ">
         {card2?.map((item) => (
-          <WrapItem key={item.id}>
-            <Card2>
+          <div key={item.id} >
+            <div className="grid h-full grid-cols-2 lg:w-[773px] md:w-full sm:w-full items-center bg-white rounded-lg md:px-4 md:py-4 p-4 lg:p-6  gap-8">
               <div>
-                <Title>{item.name}</Title>
-                <Card2Desc>{item.desc}</Card2Desc>
-                <h2 style={{ margin: "15px 0px" }}>{item.qrname}</h2>
-                <ImgConteiner>
-                  <ImgConteinerItem1 src={item.childImg1} alt="" />
+                <div className="text-[#2d3846] lg:text-4xl md:text-2xl font-bold">{item.name}</div>
+                <p className="text-[#6b7f99] text-lg mt-4 md:leading-6">{item.desc}</p>
+                <h2 className="text-2xl font-bold my-4">{item.qrname}</h2>
+                <div className="grid grid-cols-2 gap-2">
+                  <img src={item.childImg1} className="w-[136px] h-[136px]" alt="" />
                   <div>
-                    <ImgConteinerItem2 src={item.appstoe} alt="" />
-                    <ImgConteinerItem3 src={item.google_play} alt="" />
+                    <img src={item.appstoe} className="w-[200px]" alt="" />
+                    <img src={item.google_play} className="w-[165px]" alt="" />
                   </div>
-                </ImgConteiner>
+                </div>
               </div>
-              <Image src={item.image} alt="" />
-            </Card2>
-          </WrapItem>
+              <img src={item.image} alt="" className="w-full " />
+            </div>
+
+          </div>
         ))}
 
         {card2later?.map((item) => (
-          <Card key={item.id}>
-            <ImgConteinerItem2 src={item.image} />
-            <Title>{item.name}</Title>
-            <Text>{item.desc}</Text>
-            <Btn2>{item.btn}</Btn2>
-          </Card>
+          <div key={item.id} className="bg-white rounded-lg fixed  px-2 pb-6 flex flex-col items-center text-center">
+            <img src={item.image} className="w-full" alt="" />
+            <div className="text-[#2d3846] text-4xl font-bold mt-4">{item.name}</div>
+            <p className="text-[#6b7f99] text-lg mt-4 leading-7">{item.desc}</p>
+            <button className="w-[223px] h-12 text-white bg-[#40BF9C] text-lg font-bold rounded mt-6 hover:bg-[#4febbe]">
+              {item.btn}
+            </button>
+          </div>
         ))}
-      </WrapperChild>
-    </Wrapper>
+      </div>
+    </div>
   );
 }
 
--- src/Components/ThePerfect/ThePerfect.jsx
+++ src/Components/ThePerfect/ThePerfect.jsx
@@ -1,31 +1,29 @@
 import React from "react";
-import {
-  Cards,
-  Container,
-  ContainerCards,
-  Title,
-  Text,
-  Btn,
-  CardTitle,
-  Img,
-} from "./style";
 import { TheCardsData, theCardsTitle } from "../../Utils/coruselData";
 
 function ThePerfect() {
   return (
-    <Container>
-      <Title>{theCardsTitle}</Title>
-      <ContainerCards>
+    <div className="bg-white py-16 w-[100%]">
+      <h1 className="text-4xl md:text-5xl text-center text-[#2d3846] font-bold">
+        {theCardsTitle}
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 mt-12">
         {TheCardsData?.map((item, index) => (
-          <Cards key={index}>
-            <Img src={item.image} alt="" />
-            <CardTitle>{item.name}</CardTitle>
-            <Text>{item.desc}</Text>
-            <Btn>{item.btn}</Btn>
-          </Cards>
+          <div key={index} className="text-center p-4  ">
+            <img src={item.image} alt="" className="w-full" />
+            <div className=" flex flex-col h-[240px] items-center justify-between ">
+              <h2 className="text-3xl text-[#2d3846] mt-4">{item.name}</h2>
+              <p className="text-lg text-[#6b7f99]  leading-7">
+                {item.desc}
+              </p>
+              <button className="w-[223px]  h-12 bg-[#2493df] text-white text-lg font-bold rounded-md ">
+                {item.btn}
+              </button>
+            </div>
+          </div>
         ))}
-      </ContainerCards>
-    </Container>
+      </div>
+    </div>
   );
 }
 
